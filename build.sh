492f945affdce39a509e1951e4b0c589f9dcdb21
huangchenghui3 <huangchenghui3@jd.com>
2021-09-02 20:09:51 +0800

    fix: 修复编译过程

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ set -e
 function clean
 {
     # 部署用的全套代码以及node层的依赖
-    echo "Clean up..."
+    echo "--------------------------------------------"
+    echo "                  Clean up...               "
+    echo "--------------------------------------------"
     rm -rf output
 
     # 探针编译代码
@@ -13,39 +15,59 @@ function clean
     # 控制台编译代码
     rm -rf packages/pconsole/dist/build
 
-    echo "Clean up successfully!"
+    echo "--------------------------------------------"
+    echo "            Clean up successfully!          "
+    echo "--------------------------------------------"
 }
 
 function output
 {
-    echo "Create output directory."
+    echo "--------------------------------------------"
+    echo "            Create output directory.        "
+    echo "--------------------------------------------"
     mkdir -p output
 
     # 复制探针编译代码至控制台代码
-    echo "Combine compiled probe files with compiled console files."
+    echo "--------------------------------------------"
+    echo "             Combine compiled files.        "
+    echo "--------------------------------------------"
     cp -r packages/boomerang/build/* packages/pconsole/dist/build/assets/js/boomerang
     cp -r packages/pconsole/dist/build/assets/js/boomerang/plugins/* packages/pconsole/dist/build/assets/js/boomerang
 
     # 复制bin至output
-    echo "Copy bin to output folder."
+    echo "--------------------------------------------"
+    echo "          Copy bin to output folder.        "
+    echo "--------------------------------------------"
     cp -r bin output
 
     # 复制控制台编译代码(不包含node_modules)、bin至output
-    echo "Copy files to output."
+    echo "--------------------------------------------"
+    echo "              Copy files to output.         "
+    echo "--------------------------------------------"
     cp -r packages/pconsole/dist/build/* output
+    cp -r packages/pconsole/package* output
+
+    # 安装依赖
+    echo "--------------------------------------------"
+    echo "        Downloading server node_modules.    "
+    echo "--------------------------------------------"
+    yarn --cwd output --production
 
     # 执行权限
-    echo "Set priority to bin."
+    echo "--------------------------------------------"
+    echo "               Set priority to bin.         "
+    echo "--------------------------------------------"
     chmod +x output/bin/control
 }
 
 function build
 {
     # 安装依赖
-    echo "Start to install npm packages."
+    echo "--------------------------------------------"
+    echo "               Start to build.              "
+    echo "--------------------------------------------"
+    npm install
     npm run reinstall
-
-    echo "Build for production."
     npm run build
 }
 
