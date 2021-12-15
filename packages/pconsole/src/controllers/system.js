41555a51c0a116a036a63e2712c500db08cc002d
huangchenghui3 <huangchenghui3@jd.com>
2021-12-15 09:35:29 +0800

    feat: 更新系统ORM

--- packages/pconsole/src/controllers/system.js
+++ packages/pconsole/src/controllers/system.js
@@ -1,14 +1,12 @@
 import moment from 'moment'
-import sql from 'node-transform-mysql'
-import SqlString from 'sqlstring'
 import { Client } from 'elasticsearch'
 import {
     SYSTEM
 } from '../config'
 import {
     util,
-    mysql,
 } from '../tool'
+import SystemService from '../services/systemService'
 
 class user {
     //初始化对象
@@ -18,13 +16,8 @@ class user {
     async getSystemList(ctx){
         try {
             let valArr = [];
-            const sqlstr = sql
-                .table('web_system')
-                .order('create_time desc')
-                .select();
-            const result = await mysql(sqlstr);
-
-            result.forEach(item => {
+            const resps = await SystemService.getAllSystems()
+            resps.forEach(item => {
                 const {
                     id,
                     system_domain: systemDomain,
@@ -42,7 +35,7 @@ class user {
                     is_monitor_ajax: isMonitorAjax,
                     is_monitor_resource: isMonitorResource,
                     is_monitor_system: isMonitorSystem
-                } = item;
+                } = item.dataValues;
                 valArr.push({
                     id, systemDomain, systemName, script,
                     isUse, createTime, slowPageTime, slowJsTime, slowCssTime, slowImgTime,
@@ -54,7 +47,6 @@ class user {
             ctx.body = util.result({
                 data: valArr
             });
-
         } catch (err) {
             console.log(err)
             ctx.body = util.result({
@@ -65,58 +57,46 @@ class user {
         }
     }
 
-    // 根据系统ID查应用
-    async querySystemById(id) {
-        let valjson = {};
-        if (!id) {
-            return valjson
-        }
-        // 获得列表
-        const safeId = SqlString.escape(id)
-        let sqlstr = sql
-            .table('web_system')
-            .where({ id: safeId })
-            .select()
-        sqlstr = util.formatSqlstr(sqlstr, safeId);
-        const result = await mysql(sqlstr);
-        if (result.length) {
-            const {
-                id,
-                system_domain: systemDomain,
-                system_name: systemName,
-                script,
-                is_use: isUse,
-                create_time: createTime,
-                slow_page_time: slowPageTime,
-                slow_js_time: slowJsTime,
-                slow_css_time: slowCssTime,
-                slow_img_time: slowImgTime,
-                slow_ajax_time: slowAjaxTime,
-                uuid,
-                is_monitor_pages: isMonitorPages,
-                is_monitor_ajax: isMonitorAjax,
-                is_monitor_resource: isMonitorResource,
-                is_monitor_system: isMonitorSystem
-            } = result[0];
-            Object.assign(valjson, { id, systemDomain, systemName, script,
-                isUse, createTime, slowPageTime, slowJsTime, slowCssTime, slowImgTime,
-                slowAjaxTime, uuid, isMonitorPages, isMonitorPages, isMonitorAjax, isMonitorResource,
-                isMonitorSystem
-            });
-        }
-        return valjson;
-    }
-
-    //查询某个应用
+    // 根据系统ID查询系统详情
     async getItemSystem(ctx){
         try {
             const { systemId } = ctx.request.body;
-            const instance = new user()
-            const result = await instance.querySystemById(systemId)
+            let data = {};
+            if (!systemId) {
+                ctx.body = util.result({
+                    data
+                });
+                return 
+            }
+            const resps = await SystemService.getSystemById(systemId);
+            if (resps.length) {
+                const {
+                    id,
+                    system_domain: systemDomain,
+                    system_name: systemName,
+                    script,
+                    is_use: isUse,
+                    create_time: createTime,
+                    slow_page_time: slowPageTime,
+                    slow_js_time: slowJsTime,
+                    slow_css_time: slowCssTime,
+                    slow_img_time: slowImgTime,
+                    slow_ajax_time: slowAjaxTime,
+                    uuid,
+                    is_monitor_pages: isMonitorPages,
+                    is_monitor_ajax: isMonitorAjax,
+                    is_monitor_resource: isMonitorResource,
+                    is_monitor_system: isMonitorSystem
+                } = resps[0].dataValues;
+                Object.assign(data, { id, systemDomain, systemName, script,
+                    isUse, createTime, slowPageTime, slowJsTime, slowCssTime, slowImgTime,
+                    slowAjaxTime, uuid, isMonitorPages, isMonitorPages, isMonitorAjax, isMonitorResource,
+                    isMonitorSystem
+                });
+            }
             ctx.body = util.result({
-                data: result
+                data
             });
-
         } catch (err) {
             console.error(err);
             ctx.body = util.result({
@@ -131,37 +111,25 @@ class user {
     async addSystem(ctx){
         try {
             const {
-                systemName,
-                systemDomain,
+                systemName: system_name,
+                systemDomain: system_domain,
                 slowPageTime,
                 slowJsTime,
                 slowCssTime,
                 slowImgTime,
             } = ctx.request.body;
-            if(!systemName || !systemDomain){
+            if(!system_name || !system_domain) {
                 ctx.body = util.result({
                     code: 1001,
                     desc: '参数错误!'
                 });
                 return
             }
-
             const create_time = moment(new Date().getTime()).format('YYYY-MM-DD HH:mm:ss');
-            const safeSystemName = SqlString.escape(systemName);
-            const safeSystemDomain = SqlString.escape(systemDomain);
-            const safeSlowPageTime = SqlString.escape(slowPageTime || 0);
-            const safeSlowJsTime = SqlString.escape(slowJsTime || 0);
-            const safeSlowCssTime = SqlString.escape(slowCssTime || 0);
-            const safeSlowImgTime = SqlString.escape(slowImgTime || 0);
 
             // 判断应用是否存在
-            let sqlstr1 = sql
-                .table('web_system')
-                .where({ system_name: safeSystemName })
-                .select()
-            sqlstr1 = util.formatSqlstr(sqlstr1, safeSystemName);
-            const systemNameMsg = await mysql(sqlstr1);
-            if(systemNameMsg.length){
+            const systemNameResps = await SystemService.getSystemByName(system_name);
+            if(systemNameResps.length){
                 ctx.body = util.result({
                     code: 1001,
                     desc: '应用名称已存在!'
@@ -170,13 +138,8 @@ class user {
             }
 
             // 判断域名是否存在
-            let sqlstr2 = sql
-                .table('web_system')
-                .where({ system_domain: safeSystemDomain })
-                .select()
-            sqlstr2 = util.formatSqlstr(sqlstr2, safeSystemDomain);
-            const systemDomainMsg = await mysql(sqlstr2);
-            if(systemDomainMsg.length){
+            const systemDomainResps = await SystemService.getSystemByDomain(system_domain);
+            if(systemDomainResps.length) {
                 ctx.body = util.result({
                     code: 1001,
                     desc: '此域名已存在!'
@@ -186,8 +149,8 @@ class user {
 
             const timestamp = new Date().getTime();
             const token = util.signwx({
-                system_name: safeSystemName,
-                system_domain: safeSystemDomain,
+                system_name,
+                system_domain,
                 timestamp,
                 random: util.randomString()
             }).paySign;
@@ -213,30 +176,19 @@ class user {
 
             // 插入数据
             const data = {
-                system_name: safeSystemName,
-                system_domain: safeSystemDomain,
+                system_name,
+                system_domain,
                 script,
                 uuid: token,
                 create_time
             }
 
-            if(safeSlowPageTime) data.slow_page_time = safeSlowPageTime;
-            if(safeSlowJsTime) data.slow_js_time = safeSlowJsTime;
-            if(safeSlowCssTime) data.slow_css_time = safeSlowCssTime;
-            if(safeSlowImgTime) data.slow_img_time = safeSlowImgTime;
-            
-            let sqlstr3 = sql
-                .table('web_system')
-                .data(data)
-                .insert();
-            sqlstr3 = util.formatSqlstr(sqlstr3, safeSystemName);
-            sqlstr3 = util.formatSqlstr(sqlstr3, safeSystemDomain);
-            sqlstr3 = util.formatSqlstr(sqlstr3, safeSlowPageTime);
-            sqlstr3 = util.formatSqlstr(sqlstr3, safeSlowJsTime);
-            sqlstr3 = util.formatSqlstr(sqlstr3, safeSlowCssTime);
-            sqlstr3 = util.formatSqlstr(sqlstr3, safeSlowImgTime);
-
-            await mysql(sqlstr3);
+            if(slowPageTime) data.slow_page_time = slowPageTime;
+            if(slowJsTime) data.slow_js_time = slowJsTime;
+            if(slowCssTime) data.slow_css_time = slowCssTime;
+            if(slowImgTime) data.slow_img_time = slowImgTime;
+    
+            await SystemService.createSystem(data);
 
             ctx.body = util.result({
                 data: {
@@ -259,27 +211,23 @@ class user {
     // 设置系统是否需要统计数据
     async isStatisData(ctx){
         try {
-            const id    = SqlString.escape(ctx.request.body.systemId);
-            const key   = SqlString.escape(ctx.request.body.key);
-            const value = SqlString.escape(ctx.request.body.value);
+            const { systemId: id, key, value } = ctx.request.body
 
             let data = {};
-            data[key] = value;
-
-            let sqlstr = sql
-                .table('web_system')
-                .data(data)
-                .where({ id })
-                .update();
-            sqlstr = util.formatSqlstr(sqlstr, id);
-            const result = await mysql(sqlstr);
+            if (key === 'isUse') {
+                data['is_use'] = value;
+            }
+            if (!key || !value || !id) {
+                throw new Error(`Invalid parameter: id: ${id}, key: ${key}, value: ${value}`);
+            }
+            const result = await SystemService.updateSystem(id, data);
 
             ctx.body = util.result({
                 data: result
             });
 
         } catch (err) {
-            console.log(err)
+            console.error(err)
             ctx.body = util.result({
                 code: 1001,
                 desc: '系统错误!'
@@ -293,35 +241,24 @@ class user {
         try {
             const {
                 id,
-                systemName,
-                systemDomain,
-                slowPageTime,
-                slowJsTime,
-                slowCssTime,
-                slowImgTime,
-                slowAjaxTime,
-                isUse,
+                systemName: system_name,
+                systemDomain: system_domain,
+                slowPageTime: slow_page_time,
+                slowJsTime: slow_js_time,
+                slowCssTime: slow_css_time,
+                slowImgTime: slow_img_time,
+                slowAjaxTime: slow_ajax_time,
+                isUse: is_use,
                 uuid
             } = ctx.request.body;
 
-            if(!systemDomain || !systemName){
+            if(!system_domain || !system_name || !id){
                 ctx.body = util.result({
                     code: 1001,
                     desc: '参数错误!'
                 });
                 return
             }
-
-            const safeSystemName = SqlString.escape(systemName);
-            const safeSystemDomain = SqlString.escape(systemDomain);
-            const safeSlowPageTime = SqlString.escape(slowPageTime || 0);
-            const safeSlowJsTime = SqlString.escape(slowJsTime || 0);
-            const safeSlowCssTime = SqlString.escape(slowCssTime || 0);
-            const safeSlowImgTime = SqlString.escape(slowImgTime || 0);
-            const safeSlowAjaxTime = SqlString.escape(slowAjaxTime || 0);
-            const safeId = SqlString.escape(id);
-            const safeIsUse = SqlString.escape(isUse);
-            const safeuuid = SqlString.escape(uuid);
 
             const protocol = `http${SYSTEM.IS_HTTPS === 'TRUE' ? 's' : ''}`;
             const script =
@@ -342,45 +279,30 @@ class user {
                         get_user_id_callback: window.get_user_id_callback
                     });
                 </script>`;
-            let sqlstr = sql
-                .table('web_system')
-                .data({
-                    system_name: safeSystemName,
-                    system_domain: safeSystemDomain,
-                    slow_page_time: safeSlowPageTime,
-                    slow_js_time: safeSlowJsTime,
-                    slow_css_time: safeSlowCssTime,
-                    slow_img_time: safeSlowImgTime,
-                    slow_ajax_time: safeSlowAjaxTime,
-                    is_use: safeIsUse,
-                    uuid: safeuuid,
-                    script
-                })
-                .where({ id: safeId })
-                .update()
-            sqlstr = util.formatSqlstr(sqlstr, safeSystemName);
-            sqlstr = util.formatSqlstr(sqlstr, safeSystemDomain);
-            sqlstr = util.formatSqlstr(sqlstr, safeSlowPageTime);
-            sqlstr = util.formatSqlstr(sqlstr, safeSlowJsTime);
-            sqlstr = util.formatSqlstr(sqlstr, safeSlowCssTime);
-            sqlstr = util.formatSqlstr(sqlstr, safeSlowImgTime);
-            sqlstr = util.formatSqlstr(sqlstr, safeSlowAjaxTime);
-            sqlstr = util.formatSqlstr(sqlstr, safeIsUse);
-            sqlstr = util.formatSqlstr(sqlstr, safeuuid);
-            sqlstr = util.formatSqlstr(sqlstr, safeId);
-            const result = await mysql(sqlstr);
+            const data = {
+                system_name,
+                system_domain,
+                slow_page_time,
+                slow_js_time,
+                slow_css_time,
+                slow_img_time,
+                slow_ajax_time,
+                is_use,
+                uuid,
+                script
+            };
+            await SystemService.updateSystem(id, data);
 
             ctx.body = util.result({
-                data: result
+                code: 200,
+                desc: '更新成功!'
             });
-
         } catch (err) {
             console.error(err)
             ctx.body = util.result({
                 code: 1001,
                 desc: '系统错误!'
             });
-            return '';
         }
     }
 
--- packages/pconsole/src/services/systemService.js
+++ packages/pconsole/src/services/systemService.js
@@ -12,6 +12,47 @@ const {
         order: [[ 'create_time', 'DESC' ]],
       })
     }
+
+    // 根据系统ID获取系统详情
+    async getSystemById(id) {
+      return WebSystem.findAll({
+        where: {
+          id,
+        },
+      })
+    }
+
+    // 根据系统ID获取系统详情
+    async getSystemByName(system_name) {
+      return WebSystem.findAll({
+        where: {
+          system_name,
+        },
+      })
+    }
+
+    // 根据系统Domain获取系统详情
+    async getSystemByDomain(system_domain) {
+      return WebSystem.findAll({
+        where: {
+          system_domain,
+        },
+      })
+    }
+
+    // 新增系统
+    async createSystem(system) {
+      return WebSystem.create(system)
+    }
+
+    // 更新系统设置
+    async updateSystem (id, data) {
+      await WebSystem.update(data, {
+        where: {
+          id,
+        }
+      })
+    }
   }
   
   module.exports = new SystemService()
